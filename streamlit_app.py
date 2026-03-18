463459adbb0a0d9d0e67116833088c530f01600d
krishnaprashu-dot <krishnaprashu@gmail.com>
2026-03-19 07:57:50 +1100

    Fix string formatting for API request URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ if ingredients_list:
 
         # New section to display smoothiefroot nutrition information
         st.subheader(fruit_chosen + ' Nutrion Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + {search_on})  
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")  
         #st.text(smoothiefroot_response.json())
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
